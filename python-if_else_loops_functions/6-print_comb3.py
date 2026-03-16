cdc54117bc109253bd1cbd4c61bd22cc88ce5a53
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-16 12:08:42 +0000

    Python

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
-    for k in range(i+1,10):
-        num=str(i)+str(k)
+    for k in range(i+1, 10):
+        num = str(i) + str(k)
         if str(i) == '8' and str(k) == '9':
             print("{}".format(num))
         else:
             print("{}".format(num), end=", ")
-
