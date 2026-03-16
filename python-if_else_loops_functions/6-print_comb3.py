de84f3a03e54b787889397afbbec54713b81c6e5
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-16 11:57:14 +0000

    Python

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
-    for k in range(i-1, 10):
+    for k in range(i+1,10):
         num=str(i)+str(k)
-        print(num)
+        print("{}".format(num), end =",")
