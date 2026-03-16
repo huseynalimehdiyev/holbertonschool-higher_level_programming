15dca4f9e192bacad3c7327ef654c2b834a9c317
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-17 03:02:07 +0400

    Python

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     new_str = str[:]
-    return new_str[:n] + new_str[n+1:]
+    if n >= 0:
+        return new_str[:n] + new_str[n+1:]
+    else:
+        return new_str[:abs(n)] + new_str[n:]
