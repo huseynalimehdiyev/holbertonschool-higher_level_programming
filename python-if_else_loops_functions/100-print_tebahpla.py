9560367bf5704ba4687b8d68fa3a8c7193fe397b
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-17 03:56:06 +0400

    Python

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(1, 27):
-    print("{}".format((chr(65 + 26 - i))) if i % 2 == 0\
+    print("{}".format((chr(65 + 26 - i))) if i % 2 == 0
         else "{}".format((chr(123 - i))), end="")
