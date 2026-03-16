36dd0b5de2a05a49992cb3922b0041290c98b268
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-16 11:09:47 +0000

    Python

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i == 'q' or i == 'e':
-        pass
-    print("{}".format(chr(i)), end="")
+    if i != 'q' or i != 'e':
+        print("{}".format(chr(i)), end="")
