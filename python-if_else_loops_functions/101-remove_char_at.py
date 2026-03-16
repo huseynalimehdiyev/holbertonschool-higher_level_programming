f5979851cf6884333102cfd3ae46143e48b0f0c2
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-17 03:04:25 +0400

    Python

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -3,5 +3,5 @@ def remove_char_at(str, n):
     new_str = str[:]
     if n >= 0:
         return new_str[:n] + new_str[n+1:]
-    else:
-        return new_str[:abs(n)] + new_str[n:]
+    return new_str
+print(remove_char_at("Chicago", -3))
