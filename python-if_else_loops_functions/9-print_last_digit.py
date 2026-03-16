cc6fac28ea67336b2c19cff825906d5627c15e91
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-16 12:21:53 +0000

    Python

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number >= 0:
-        return number = number % 10
-    else:
-        return number = 10 - number % 10
+    last_digit = abs(number) % 10
+    return last_digit
