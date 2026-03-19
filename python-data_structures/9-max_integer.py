557051b41db80b368df9d89f1b969f52b8e0b9bf
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-20 02:59:43 +0400

    Python

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -6,4 +6,4 @@ def max_integer(my_list=[]):
     for i in range(1,len(my_list)):
         if my_list[i] > max_value:
             max_value = my_list[i]
-    return "Max: {:d}".format(max_value)
+    return max_value
