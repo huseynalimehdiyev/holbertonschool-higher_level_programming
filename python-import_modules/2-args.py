1946a25c8b30b071e88970dab50f2af8d1a13724
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-20 00:26:52 +0400

    Python

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
 if len(args) == 1:
         print(f"1 argument:")
 elif len(args) == 0:
-    print(f"0 argument.")
+    print(f"0 arguments.")
 else:
         print(f"{len(args)} arguments:")
 for i in range(0,len(args)):
-        print(f"{i+1}: {args[i]}")
+        print(f"{i+1}: {args[i]}")q
