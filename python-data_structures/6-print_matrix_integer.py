9dfaed462ef33d7870aa8e4746e32789608c996e
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-20 02:32:42 +0400

    Python

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,4 @@ def print_matrix_integer(matrix=[[]]):
         for j in range (len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end =" " 
                   if j < len(matrix[i])-1 else "")
-        if i < len(matrix)-1:
-            print()
+        print()
