85ceee2fe7b931274b973b32582cec4baa201151
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-20 01:42:20 +0400

    Python

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,5 +3,5 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or len(my_list) <= idx:
         return my_list
     else:
-        my_list[idx] == element
+        my_list[idx] = element
         return my_list
