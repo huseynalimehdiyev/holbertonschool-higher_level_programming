d286374ff57da290b7d3a1fe668d71b26bc0bd5c
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-21 02:30:55 +0400

    Python tasks

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+if __name__ == "__main__":
+    if len(sys.argv[1:]) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = sys.agv[1]
+    operator = sys.arv[2]
+    b = sys.argv[3]
