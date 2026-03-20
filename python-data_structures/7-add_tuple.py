4648cf9c2b2b6c9162da64a47df270679ee72f6b
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-20 16:54:38 +0400

    Python Tasks

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2 and len(tuple_b) < 2:
-        return tuple_a[:2]
-    elif len(tuple_a) > 2 and len(tuple_b) > 2:
-            return tuple_a[:2]+tuple_b[:2]
-    else:
-        return tuple_a + tuple_b
+    tuple_a = (tuple_a + (0, 0))[:2]
+    tuple_b = (tuple_b + (0, 0))[:2]
+    return (tuple_a[0] + tuple_b[0],
+            tuple_a[1] + tuple_b[1])
+            
 tuple_a = (1, 89)
 tuple_b = (88, 11)
 new_tuple = add_tuple(tuple_a, tuple_b)
