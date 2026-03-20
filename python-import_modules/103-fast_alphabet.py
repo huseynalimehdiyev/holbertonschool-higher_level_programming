35eec08b6e308f62f0e0678150e14e0bec9b5a14
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-20 19:03:26 +0400

    Python Tasks

---
+++ python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from printalphabet import alphabet
+print(alphabet())
---
+++ python-import_modules/printalphabet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def alphabet():
+    c=""
+    for i in range(65, 91):
+        c+=chr(i)
+    return c
