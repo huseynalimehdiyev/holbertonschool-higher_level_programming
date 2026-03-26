3bab652f5d8a5249f150a71f58cf77ac3031b002
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-27 01:51:00 +0400

    Python Tasks

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dict = {}
     for i in a_dictionary.keys():
-        new_dict[i] = new_dict[i] * 2
-    return a_dictionary
+        new_dict[i] = a_dictionary[i] * 2
+    return new_dict
