b74e86f533e83ce626d29b4b2bd2a3acba512db4
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-27 00:48:09 +0400

    Python Tasks

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,5 +3,5 @@ def uniq_add(my_list=[]):
     new_list = []
     for i in my_list:
         if my_list.count(i) == 1:
-            new_list.appemd(i)
+            new_list.append(i)
     return new_list
