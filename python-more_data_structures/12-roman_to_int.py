0025979c99aa4661df3e4629567619e31c26a2e3
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-27 02:49:17 +0400

    Python Tasks

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     num = 0
     roman_num = {"I": 1, "V" : 5, "X" : 10, "L": 50, "C" : 100, "D" : 500, "M" : 1000}
-    for i in range (1, len(roman_string)-1):
+    for i in range (len(roman_string)):
         if roman_string[i] < roman_string[i+1]:
             num = roman_num[roman_string[i+1]] - roman_num[roman_string[i]]
         else:
