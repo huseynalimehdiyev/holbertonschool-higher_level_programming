176c37f1ca91dda3bfd36ba46f4d57f4ad35e839
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-27 00:52:09 +0400

    Python Tasks

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    sum = 0
-    for i in my_list:
-        if my_list.count(i) == 1:
-            sum += i
-    return sum
+    return sum(set(my_list))
