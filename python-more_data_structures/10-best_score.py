150504aa753c521c0990decac8e7b5b18a941817
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-27 01:55:48 +0400

    Python Tasks

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     max_val = 0
     for i in a_dictionary.keys():
         if a_dictionary[i] > max_val:
-            max_val = a_dictionary[i]
+            max_val == a_dictionary[i]
     return a_dictionary[max_val]
