b4b63eeb0ffcb23ce207fc1a4d3141cfed03134c
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-27 00:36:15 +0400

    Python Tasks

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    new_matrix = []
     for i in range(len(matrix)):
-        for j in range(len(i)):
-            return j**2
-squared_matrix=list(map(square_matrix_simple,matrix))
+        new_row = []
+        for j in range(len(matrix[i])):
+            new_row.append(matrix[i][j]**2)
+        new_matrix.append(new_row)
+    return new_matrix
