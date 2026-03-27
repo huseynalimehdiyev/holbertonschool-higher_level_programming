2d1c1ea0bc768daf03e134ee2f0f15968cecf8e2
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-28 02:00:21 +0400

    Python Tasks

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,6 +2,6 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
-            print(my_list[i], end=" ")
+            print(my_list[i], end="")
     except IndexError:
         print("Index out of range")
