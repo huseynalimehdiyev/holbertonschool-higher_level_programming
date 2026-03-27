ad6c463b9e497bc4e20914fcc6a6c5bb33a299e1
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-28 02:20:12 +0400

    Python Tasks

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
             count += 1
-        except(IndexError, TypeError):
+        except (IndexError, TypeError):
             break
-    print("")
+    print()
     return count
