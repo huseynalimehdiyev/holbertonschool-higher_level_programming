735036d05a692d6d906446e7fb606f68e249e98a
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-29 03:52:53 +0400

    Python Tasks

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -18,3 +18,6 @@ def matrix_divided(matrix, div):
     
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    
+    new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
+    return new_matrix
