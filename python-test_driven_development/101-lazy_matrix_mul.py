6a90545a0aff08aaba8faaaa25b49897522065cf
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-29 17:23:42 +0400

    Python Tasks

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -13,7 +13,6 @@ def lazy_matrix_mul(m_a, m_b):
     Returns:
         list of lists: result of matrix multiplication
     """
-    # Validate types
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
@@ -26,21 +25,20 @@ def lazy_matrix_mul(m_a, m_b):
         raise ValueError("m_a can't be empty")
     if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
-    # Validate elements
     for row in m_a:
         if not all(isinstance(el, (int, float)) for el in row):
             raise TypeError("m_a should contain only integers or floats")
     for row in m_b:
         if not all(isinstance(el, (int, float)) for el in row):
             raise TypeError("m_b should contain only integers or floats")
-    # Validate rectangular
     row_len_a = len(m_a[0])
     if any(len(row) != row_len_a for row in m_a):
         raise TypeError("each row of m_a must be of the same size")
     row_len_b = len(m_b[0])
     if any(len(row) != row_len_b for row in m_b):
         raise TypeError("each row of m_b must be of the same size")
-    # Validate multiplication compatibility
+    if row_len_a != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
     
     
     # Use NumPy to multiply matrices
