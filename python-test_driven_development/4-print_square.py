5d6c9981aaafb32ed34a44a7a315414d174d8310
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-29 16:28:04 +0400

    Python Tasks

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module provides a function to print a square."""
+
+
+def print_square(size):
+    """Prints a square with the character #."""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
