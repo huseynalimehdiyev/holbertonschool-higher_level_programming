31b2f24bc319f209e27f9239d3369cbe0a8bebdc
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-29 16:47:09 +0400

    Python Tasks

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module provides a function that prints text with indentation."""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after '.', '?' and ':'."""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in ".:?":
+            print("\n")
+            i += 1
+
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+        i += 1
