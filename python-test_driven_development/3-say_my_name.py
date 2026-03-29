f6846813b233a29d990e86ad80c9e5828c92c325
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-29 16:15:51 +0400

    Python Tasks

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,10 +3,8 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints 'My name is <first_name> <last_name>'.
+    """Prints 'My name is <first_name> <last_name>'."""
 
-    first_name and last_name must be strings."""
-    
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
