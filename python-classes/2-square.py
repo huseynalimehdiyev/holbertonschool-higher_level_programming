2219e25abe3a24277d5d193d8f7ae318878c832d
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-02 01:01:50 +0400

    Python tasks

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,8 +6,7 @@ class Square:
     """Defines a square."""
 
     def __init__(self, size=0):
-        self.__size = size
-   
+        self.__size = size   
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
