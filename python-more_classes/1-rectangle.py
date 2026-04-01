6122d92914210eb4791d0f9994ad4b7858e64fd2
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-02 02:17:43 +0400

    Python tasks

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        
+
         self.__width = value
 
     @height.setter
@@ -32,5 +32,5 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        
+
         self.__height = value
