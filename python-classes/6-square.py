47f8455d6a05cb1ff9aa589f68098586cb2f6b79
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-02 01:42:51 +0400

    Python tasks

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -42,5 +42,10 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range(self.__size):
-                print("#" * self.__size)
+            for _ in range(self.__position[1]):
+                print()
+
+        # print square with horizontal offset
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
+        
