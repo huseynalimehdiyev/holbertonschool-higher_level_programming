2041c9966bd66e7be43e814f19b860aff42453dd
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-02 01:22:04 +0400

    Python tasks

---
+++ python-classes/5-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+    
+    def my_print(self):
+        for i in range(self.value):
+            print("#" * self.value)
