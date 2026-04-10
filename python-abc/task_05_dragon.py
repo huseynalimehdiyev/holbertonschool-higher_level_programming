001695668ab3373ec0dc2d2f60d0d42be38f6942
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-11 03:42:04 +0400

    Python

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -12,6 +12,11 @@ class FlyMixin:
         print("The creature flies!")
 
 class Dragon(SwimMixin, FlyMixin):
+    def swim(self):
+        print("The creature swims!")
+
+    def fly(self):
+        print("The creature flies!")
   
     def roar(self):
         print("The dragon roars!")
