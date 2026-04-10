10cba37bfba5b6c3367b6efc2e592dffec320306
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-11 02:08:31 +0400

    Python

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,7 +13,6 @@ class BaseGeometry:
         """Validate that value is a positive integer."""
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
-        
+
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-    
