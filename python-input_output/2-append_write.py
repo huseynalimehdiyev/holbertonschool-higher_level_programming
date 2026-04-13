106a69d078a49b65cd71d576e75c047679746f52
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-14 01:04:35 +0400

    Python Tasks

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 " a string to a text file"
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     "Write smth"
     with open(filename, 'a', encoding='utf-8') as f:
         return f.write(text)
