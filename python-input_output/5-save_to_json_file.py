47ae46676ec3036113e27a52533ddf4dbf2a37c5
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-14 01:16:12 +0400

    Python Tasks

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """Function"""
     with open(filename, 'w', encoding="utf-8") as f:
-        return json.dump(f,my_obj)
+        return json.dump(my_obj, f)
