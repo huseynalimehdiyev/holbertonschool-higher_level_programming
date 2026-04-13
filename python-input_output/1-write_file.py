df5b0905881d6fd6df98b64e9f283576a618ba08
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-14 00:55:00 +0400

    Python Tasks

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+" a string to a text file"
+
+
+def write_file(filename="", text=""):
+    with open(filename, 'r', encoding='utf-8') as f:
+        count = 0
+        for strs in text:
+            f.write(strs)
+            count+=1
