38187a628a641354bf2752ed9b17f1cda11e5916
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-17 02:12:23 +0400

    Tasks

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Interview question"""
+"""Module that contains function pascal_triangle"""
 
 
 def pascal_triangle(n):
     """Find Pascal triangle"""
-    
+
     if n <= 0:
         return []
     else:
