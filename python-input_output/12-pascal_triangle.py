66f6ba05b51ff7b08ec41abe79f0c741d1a28b99
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-19 20:48:41 +0400

    Python Tasks

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -3,13 +3,19 @@
 
 
 def pascal_triangle(n):
-    """Find Pascal triangle"""
-
     if n <= 0:
         return []
-    else:
-        triangle = []
-        for i in range(n):
-            row = [1]
 
-    return 
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
