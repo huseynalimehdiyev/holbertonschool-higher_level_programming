fdf14145ff4c83d1bb660afac8371e5a9dce327d
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-19 21:25:00 +0400

    Python Tasks

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Shebang"""
+
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+    
+    def display(self):
+        print("Name: {}\nAge: {}\nIs Student: {}".format(self.name, self.age, self.is_student))
+    
+    def serialize(self, filename):
+        try:
+            with open(filename, 'wb', encoding = 'utf-8') as f:
+                pickle.dump(self, f)
+        
+        except Exception:
+            return None
+    
+    @classmethod 
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'rb', encoding = 'utf-8') as f:
+                return pickle.load(f)
+        except Exception:
+            return None
+        
