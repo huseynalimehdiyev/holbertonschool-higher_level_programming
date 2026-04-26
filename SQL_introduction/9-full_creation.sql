8fbdce7691724871da28844c49f25470fc01eecf
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-26 22:54:53 +0000

    SQL Tasks

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- CREATE and INSERT
+CREATE TABLE IF EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+INSERT INTO second_table VALUES(id = 1, name = "John", score = 10);
+INSERT INTO second_table VALUES(id = 2, name = "Alex", score = 3);
+INSERT INTO second_table VALUES(id = 3, name = "Bob", score = 14);
+INSERT INTO second_table VALUES(id = 4, name = "George", score = 8);
