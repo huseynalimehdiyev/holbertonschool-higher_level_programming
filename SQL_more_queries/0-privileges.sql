ebdc028a6f222e105bb4a88239e4fe4acb1480c0
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-28 13:39:20 +0000

    SQL Tasks

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,4 @@
--- lists all privileges of the MySQL users
--- user_0d_1 istifadəçisinin imtiyazlarını siyahıla
+-- lists privileges for user_0d_1 and user_0d_2 on localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- user_0d_2 istifadəçisinin imtiyazlarını siyahıla
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
