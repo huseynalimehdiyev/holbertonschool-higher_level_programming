af0885a748bdecdd0774039ece8b9d9bcfaa9735
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-28 13:39:57 +0000

    SQL Tasks

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,13 @@
--- lists privileges for user_0d_1 and user_0d_2 on localhost
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- lists privileges safely
 
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT IF(
+    EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost'),
+    (SHOW GRANTS FOR 'user_0d_1'@'localhost'),
+    NULL
+);
+
+SELECT IF(
+    EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost'),
+    (SHOW GRANTS FOR 'user_0d_2'@'localhost'),
+    NULL
+);
