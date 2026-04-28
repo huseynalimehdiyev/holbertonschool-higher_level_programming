94de041ffa03d585ab754b35f97a18a389df99cc
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-28 13:15:13 +0000

    SQL Tasks

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,8 @@
 -- lists all privileges of the MySQL users
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
