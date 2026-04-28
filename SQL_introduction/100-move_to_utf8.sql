df5f7a78331aaa031a0042fd38c924f204f784f2
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-04-28 12:10:03 +0000

    SQL Tasks

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -4,7 +4,7 @@ CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
-CONVERT TO CAHARACTER SET utf8mb4
+CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
