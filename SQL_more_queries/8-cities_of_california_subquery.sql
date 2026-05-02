333ef4ecfa78321ad2618376381c48ede178e00f
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-05-02 21:12:07 +0000

    SQL Tasks

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,4 @@
 -- select
-SELECT id,name FROM states WHERE state_id=(SELECT id FROM states WHERE name='California');
+SELECT id,name FROM states
+WHERE state_id=(SELECT id FROM states WHERE name='California')
+ORDER BY id;
