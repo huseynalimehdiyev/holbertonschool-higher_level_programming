4d79b90819009c893a1fc338cb2931d5780387b4
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-05-02 14:34:50 +0000

    SQL Tasks

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,13 @@
 --create 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(256) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	state_id INT NOT NULL,
