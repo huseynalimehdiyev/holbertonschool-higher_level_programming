a0ac002f98d233e79cf59904950e617fb6d71773
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-05-02 21:03:31 +0000

    SQL Tasks

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,14 @@
--- create 
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- Switch to the database context
 USE hbtn_0d_usa;
 
+-- Create the table cities
+-- The FOREIGN KEY references hbtn_0d_usa.states(id) to ensure consistency
 CREATE TABLE IF NOT EXISTS cities (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES states(id)
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
